3f7d99b94e5151be5c4f4a906cbf3411970d96c6
rickychan0611 <ric0611@gmail.com>
2021-07-06 12:57:30 -0700

    ignore eslint

--- next.config.js
+++ next.config.js
@@ -1,15 +1,16 @@
-const nextTranslate = require('next-translate')
-
-// module.exports = nextTranslate()
+const nextTranslate = require('next-translate');
 
 module.exports = nextTranslate({
   experimental: {
     scrollRestoration: true
+  },
+  future: {
+    webpack5: false
+  },
+  images: {
+    domains: ['admin.peacefulmallrestaurant.com']
+  },
+  eslint: {
+    ignoreDuringBuilds: true
   }
 })
-
-// module.exports = {
-//   future: {
-//     webpack5: false,
-//   },
-// }
